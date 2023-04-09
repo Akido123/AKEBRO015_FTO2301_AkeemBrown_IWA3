3a9a2fa0504a32c1ebd4dcdb5afaadf1cf614642
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-04-09 11:35:21 +0200

    Update: script.js imports from other js files aswell as import a const as the file name.

--- Challenge 2/script.js
+++ Challenge 2/script.js
@@ -1,3 +1,11 @@
 // scripts.js
 
-console.log('Roles:', nwabisa.role, johannes.role, alex.role)
+import "./nwabisa.js"
+import "./johannes.js"
+import "./alex.js"
+
+import {role as nwabisa} from "./nwabisa.js"
+import {role as alex} from "./alex.js"
+import {role as johannes} from "./johannes.js"
+
+console.log('Roles:', nwabisa + ", " + alex + ", " + johannes)
