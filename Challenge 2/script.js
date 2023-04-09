4e53fc1b93e626517da3000ba999712d9e21d71c
Akido123 <117109568+Akido123@users.noreply.github.com>
2023-04-09 11:39:36 +0200

    Update: removed module files and import

--- Challenge 2/script.js
+++ Challenge 2/script.js
@@ -1,9 +1,5 @@
 // scripts.js
 
-import "./nwabisa.js"
-import "./johannes.js"
-import "./alex.js"
-
 import {role as nwabisa} from "./nwabisa.js"
 import {role as alex} from "./alex.js"
 import {role as johannes} from "./johannes.js"
